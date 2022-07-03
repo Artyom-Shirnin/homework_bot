c5f95213b2405b0187181aba6813f4936cdd1fe7
Артём Ширнин <artyom.shirnin@yandex.ru>
2022-07-03 13:48:26 +1000

    Правки после ревью

--- homework.py
+++ homework.py
@@ -131,8 +131,8 @@ def main():
 
 if __name__ == '__main__':
     logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s, %(levelname)s, %(message)s',
-    filename='program.log'
-)
+        level=logging.INFO,
+        format='%(asctime)s, %(levelname)s, %(message)s',
+        filename='program.log'
+    )
     main()
