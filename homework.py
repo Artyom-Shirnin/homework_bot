0eb0c486640cf02f75a7bd74494f7fe684e14297
Артём Ширнин <artyom.shirnin@yandex.ru>
2022-07-03 13:55:39 +1000

    Правки после ревью

--- homework.py
+++ homework.py
@@ -15,7 +15,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 10
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
